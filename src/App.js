227d6ed98197a949112bb3c0f122139cfa50fdf6
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-08-23 15:25:18 +0100

    Functionality to display paragraphs based on count

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setText(data);
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
 
   return (
@@ -26,7 +33,9 @@ function App() {
         </button>
       </form>
       <article className="lorem-text">
-        {}
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
