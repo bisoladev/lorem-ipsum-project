b51c35018f91a7f438431a635835508ffebfab26
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-08-23 14:58:29 +0100

    Created article and button elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React, { useState } from "react";
 import data from "./data";
 function App() {
-  return <h2>lorem ipsum project setup</h2>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, odit!
+        </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, odit!
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default App;
